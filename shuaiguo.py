8a5b3925c181b9ec775584c5fc6870a3baf4ae7a
woshoxxx <84318882@qq.com>
2021-09-19 18:52:06 +0800

    change headers

--- shuaiguo.py
+++ shuaiguo.py
@@ -3,20 +3,22 @@ import os
 import time
 
 headers = {
-    'Host': 'shuaiguo.club',
-    'Connection': 'keep-alive',
-    'sec-ch-ua': '"Chromium";v="88", "Google Chrome";v="88", ";Not A Brand";v="99"',
-    'X-Requested-With': 'XMLHttpRequest',
-    'sec-ch-ua-mobile': '?0',
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.96 Safari/537.36',
-    'Accept': '*/*',
-    'Sec-Fetch-Site': 'same-origin',
-    'Sec-Fetch-Mode': 'cors',
-    'Sec-Fetch-Dest': 'empty',
-    'Referer': 'https://shuaiguo.club/home.php?mod=task',
-    'Accept-Encoding': 'gzip, deflate, br',
-    'Accept-Language': 'zh-CN,zh;q=0.9',
-    'Cookie': ''
+    "Accept": "*/*"
+    "Accept-Encoding": "gzip, deflate, br"
+    "Accept-Language": "zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6"
+    "Connection": "keep-alive"
+    "Cookie":""
+    "Host": "bbs.sgguo.com"
+    "Referer": "https://bbs.sgguo.com/forum.php"
+    "sec-ch-ua": '"Microsoft Edge";v="93", " Not;A Brand";v="99", "Chromium";v="93"'
+    "sec-ch-ua-mobile": "?0"
+    "sec-ch-ua-platform": "Windows"
+    "Sec-Fetch-Dest": "empty"
+    "Sec-Fetch-Mode": "cors"
+    "Sec-Fetch-Site": "same-origin"
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.82 Safari/537.36 Edg/93.0.961.52"
+    "X-Requested-With": "XMLHttpRequest"
+#    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.96 Safari/537.36',
 }
 
 headers['Cookie'] = os.environ.get('shuaiguo_cookie')
