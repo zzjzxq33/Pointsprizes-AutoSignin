2cf7f3ae5ad8c043e2edc47b189d7930ad5c1a56
woshoxxx <84318882@qq.com>
2021-09-19 18:59:15 +0800

    add kuohao

--- shuaiguo.py
+++ shuaiguo.py
@@ -3,21 +3,21 @@ import os
 import time
 
 headers = {
-    "Accept": "*/*"
-    "Accept-Encoding": "gzip, deflate, br"
-    "Accept-Language": "zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6"
-    "Connection": "keep-alive"
-    "Cookie":""
-    "Host": "bbs.sgguo.com"
-    "Referer": "https://bbs.sgguo.com/forum.php"
-    "sec-ch-ua": '"Microsoft Edge";v="93", " Not;A Brand";v="99", "Chromium";v="93"'
-    "sec-ch-ua-mobile": "?0"
-    "sec-ch-ua-platform": "Windows"
-    "Sec-Fetch-Dest": "empty"
-    "Sec-Fetch-Mode": "cors"
-    "Sec-Fetch-Site": "same-origin"
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.82 Safari/537.36 Edg/93.0.961.52"
-    "X-Requested-With": "XMLHttpRequest"
+    "Accept": "*/*",
+    "Accept-Encoding": "gzip, deflate, br",
+    "Accept-Language": "zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6",
+    "Connection": "keep-alive",
+    "Cookie":"",
+    "Host": "bbs.sgguo.com",
+    "Referer": "https://bbs.sgguo.com/forum.php",
+    "sec-ch-ua": '"Microsoft Edge";v="93", " Not;A Brand";v="99", "Chromium";v="93"',
+    "sec-ch-ua-mobile": "?0",
+    "sec-ch-ua-platform": "Windows",
+    "Sec-Fetch-Dest": "empty",
+    "Sec-Fetch-Mode": "cors",
+    "Sec-Fetch-Site": "same-origin",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.82 Safari/537.36 Edg/93.0.961.52",
+    "X-Requested-With": "XMLHttpRequest",
 #    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.96 Safari/537.36',
 }
 
